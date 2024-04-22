41b18f1b4b2574383b69a567687f42172feffc02
heeyeonyune <148155351+heeyeonyune@users.noreply.github.com>
2024-04-22 20:01:42 +0100

    update microprocessor assignment

---
+++ Micro_GameProject/prbs.h
@@ -0,0 +1,6 @@
+
+#include <stdint.h>
+
+uint32_t my_random(uint32_t lower, uint32_t upper);
+uint32_t prbs(void);
+
---
+++ Micro_GameProject/serial.h
@@ -0,0 +1,6 @@
+int dataAvailable(void);
+void initSerial();
+void eputchar(char c);
+char egetchar();
+void eputs(char *String);
+void printDecimal(int32_t Value);
